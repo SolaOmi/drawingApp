40de1235738da6a84fbce37880ec7531cb01f277
sola <solaocodes@gmail.com>
2019-07-24 16:55:53 -0400

    Highlight the icon of the currently active panel

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -9,28 +9,28 @@ export default function NavBar({activeTool, setActiveTool}) {
 	return (
 		<div className="nav-bar">
 			<div
-				className="nav-bar__tool"
+				className={"nav-bar__tool" + (activeTool === "pen" ? " active" : "")}
 				onClick={e => setActiveTool("pen")}
 				title="Pen"
 			>
 				<PenIcon />
 			</div>
 			<div
-				className="nav-bar__tool"
+				className={"nav-bar__tool" + (activeTool === "eraser" ? " active" : "")}
 				onClick={e => setActiveTool("eraser")}
 				title="Eraser"
 			>
 				<EraserIcon />
 			</div>
 			<div
-				className="nav-bar__tool"
+				className={"nav-bar__tool" + (activeTool === "stamp" ? " active" : "")}
 				onClick={e => setActiveTool("stamp")}
 				title="Stamp"
 			>
 				<StampIcon />
 			</div>
 			<div
-				className="nav-bar__tool"
+				className={"nav-bar__tool" + (activeTool === "download" ? " active" : "")}
 				onClick={e => setActiveTool("download")}
 				title="Download"
 			>

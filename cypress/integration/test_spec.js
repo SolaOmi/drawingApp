00c8054f6fbfe76009e200230ee78f28f76909fb
sola <solaocodes@gmail.com>
2019-07-25 12:54:40 -0400

    Add more cypress integration tests
    Edit typo in first test name. Check that different line types can be
    clicked in the Pen panel. Check for all the elements that should be
    present in the default state of the Eraser, Stamp, and Download panels.

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,14 +1,36 @@
 /* global cy */
 
 describe("Drawing App", function () {
-	it("Visits the app and lods the canvas", function () {
+	it("Visits the app and loads the canvas", function () {
 		cy.visit("");
 		cy.title().should("contain", "ST Apprentice Challenge");
 		cy.get("h1").should("contain", "Pen");
 		cy.get("canvas").should("exist");
 	});
-	it("Loads new tool panel when clicked", function () {
+	it("Changes line type when clicked", function () {
+		cy.get(".line-types__option").eq(1).click()
+			.should("contain", "Dashed");
+		cy.get(".line-types__option").eq(2).click()
+			.should("contain", "Dotted");
+		cy.get(".line-types__option").eq(0).click()
+			.should("contain", "Solid");
+	});
+	it("Loads Eraser panel when clicked", function () {
 		cy.get(".nav-bar__tool").eq(1).click();
 		cy.get("h1").should("contain", "Eraser");
+		cy.get("input[type=range]").should("have.value", "10");
+		cy.get("#clear-btn").should("contain", "Clear Canvas");
+	});
+	it("Loads Stamp panel when clicked", function () {
+		cy.get(".nav-bar__tool").eq(2).click();
+		cy.get("h1").should("contain", "Stamp");
+		cy.get("input[type=range]").should("have.value", "200");
+		cy.get("input[type=file]").should("exist");
+		cy.get("#uploaded-image").should("exist");
+	});
+	it("Loads Download panel when clicked", function () {
+		cy.get(".nav-bar__tool").eq(3).click();
+		cy.get("h1").should("contain", "Download");
+		cy.get("#download-btn").should("contain", "Download Image");
 	});
 });

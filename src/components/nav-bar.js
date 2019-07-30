630fe5ba8454ea18af30410a6fc698ffef55ed20
sola <solaocodes@gmail.com>
2019-07-30 19:36:47 -0400

    Replace custom SVG icons with Font Awesome icons

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,10 +1,5 @@
 import React from "react";
 
-import PenIcon from "./icons/pen-icon";
-import EraserIcon from "./icons/eraser-icon";
-import StampIcon from "./icons/stamp-icon";
-import SaveIcon from "./icons/save-icon";
-
 export default function NavBar({activeTool, setActiveTool}) {
 	return (
 		<div className="nav-bar">
@@ -13,28 +8,28 @@ export default function NavBar({activeTool, setActiveTool}) {
 				onClick={e => setActiveTool("pen")}
 				title="Pen"
 			>
-				<PenIcon />
+				<i className="fas fa-pen"></i>
 			</div>
 			<div
 				className={"nav-bar__tool" + (activeTool === "eraser" ? " active" : "")}
 				onClick={e => setActiveTool("eraser")}
 				title="Eraser"
 			>
-				<EraserIcon />
+				<i className="fas fa-eraser"></i>
 			</div>
 			<div
 				className={"nav-bar__tool" + (activeTool === "stamp" ? " active" : "")}
 				onClick={e => setActiveTool("stamp")}
 				title="Stamp"
 			>
-				<StampIcon />
+				<i className="fas fa-stamp"></i>
 			</div>
 			<div
 				className={"nav-bar__tool" + (activeTool === "download" ? " active" : "")}
 				onClick={e => setActiveTool("download")}
 				title="Download"
 			>
-				<SaveIcon />
+				<i className="far fa-save"></i>
 			</div>
 		</div>
 	);
